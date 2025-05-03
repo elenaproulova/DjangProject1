c87beb9bd7bfc6f412c087351097c27b61ba06a1
elenaproulova <elenkaf2233@gmail.com>
2025-05-03 18:50:04 +0500

    add hw

--- DJproject1/news/models.py
+++ DJproject1/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class News_post(models.Model):
+    title = models.CharField('Название новости', max_length=50)
+    short_description = models.CharField('Краткое описание новости', max_length=200)
+    text = models.TextField('Новость')
+    pub_date = models.DateTimeField('Дата публикации')
+    author = models.CharField('Автор', max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
